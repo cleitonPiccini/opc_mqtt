66c2d036b6b29ddba6f3e4e67680dd35838c0452
Cleiton Piccini <cleiton.cleitonpiccini@gmail.com>
2022-05-25 00:21:55 -0300

    server e client opca ua

---
+++ Client.py
@@ -0,0 +1,54 @@
+# This code from https://github.com/FreeOpcUa
+#from opcua import Client
+import time
+import sys
+sys.path.insert(0, "..")
+
+import opcua
+
+"""class SubHandler(opcua.SubscriptionHandler):
+    
+   # Client to subcsription. It will receive events from server
+    
+    def __init__(self, *args):
+        opcua.SubscriptionHandler.__init__(self, *args)
+
+    def data_change(self, handle, node, val, attr):
+        print("Python: New data change event", handle, node, val, attr)
+
+    def event(self, handle, event):
+        print("Python: New event", handle, event)
+"""
+
+if __name__ == "__main__":
+    client = opcua.Client("opc.tcp://192.168.1.27:4840")
+    try:
+        # connecting!
+        client.connect()
+        # Client has a few methods to get proxy to UA nodes that should always be in address space
+        root = client.get_root_node()
+        myData1 = root.get_child(["0:Objects", "2:Objeto", "2:Variavel"])
+        myDataDatetime = root.get_child(["0:Objects", "2:Objeto", "2:MyDataDatetime"])
+        obj = root.get_child(["0:Objects", "2:Objeto"])
+        #teste = client.get_name_spece()
+        uri = "Teste Server"
+        idx = client.get_namespace_index(uri)
+        
+        print("Teste Objects is: ", client.get_objects_node())
+        objects = client.get_objects_node()
+        print("Teste Children of objects are: ", objects.get_children())
+
+        print("meu Objeto é: ", obj)
+        print("valor da Variavel é: ", myData1)
+        print("myDataDatetime is: ", myDataDatetime)
+        print("valor do idx = ", idx)
+        while True:
+            #client.set_node("ns=2;i=2").set_value(10)
+            count = myData1.get_value()
+            count += 0.1
+            myData1.set_value(count)
+            print("myData1 = %4.1f" %client.get_node("ns=2;i=2").get_value())
+            print("myDataDatetime = ", client.get_node("ns=2;i=3").get_value().strftime("%Y-%m-%d 	%H:%M:%S"))
+            time.sleep(2)            
+    finally:
+        client.disconnect()
---
+++ Server.py
@@ -0,0 +1,47 @@
+# This code from https://github.com/FreeOpcUa
+from opcua import ua, Server
+import sys
+import time
+import datetime
+sys.path.insert(0, "..")
+
+if __name__ == "__main__":
+    # setup our server
+    server = Server()
+    server.set_endpoint("opc.tcp://192.168.1.27:4840")
+    # setup our own namespace, not really necessary but should as spec
+    uri = "Teste Server"
+    idx = server.register_namespace(uri)
+    # get Objects node, this is where we should put our nodes
+    objects = server.get_objects_node()
+    # populating our address space
+    myobj = objects.add_object(idx, "Objeto")
+    myData1 = myobj.add_variable(idx, "Variavel", 0)
+    myDataDatetime = myobj.add_variable(idx, "MyDataDatetime", 0)
+    myData1.set_writable()    # Set MyVariable to be writable by clients
+    myDataDatetime.set_writable()    # Set MyVariable to be writable by clients
+    # starting!
+    server.start()
+    try:
+        count = 0
+        old_count = 0
+
+        count = myData1.get_value()
+        myData1.set_value(count)
+
+        old_count = myData1.get_value()
+        
+        while True:
+            time.sleep(2)
+            #
+            count = myData1.get_value()
+            if (count != old_count):
+                old_count = count
+                print("Mudou o count = %4.1f" %count)
+
+            #count += 0.1
+            myDataDatetime.set_value(datetime.datetime.now())
+            #myData1.set_value(count)
+    finally:
+        #close connection, remove subcsriptions, etc
+        server.stop()
